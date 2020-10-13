ee94f1ceeb71b1ba6c8cccdf87c6046ad9d58792
Gabriel Ribeiro <gabriel.ribeiro@ipremios.com>
2020-10-12 21:09:23 -0300

    Initializing

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+import "./global.css";
+import "./pages/Landing/Landing.css";
+
+import logoImg from "./assets/images/logo.svg";
+
+import { FiArrowRight } from "react-icons/fi";
+
+function App() {
+  return (
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy" />
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Rio de Janeiro</strong>
+          <span>RJ</span>
+        </div>
+
+        <a href="" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
